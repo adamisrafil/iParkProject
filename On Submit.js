1b5f60654c0a6e1ee0f16111641d63f9b6812894
adamisrafil <adam.israfil@gmail.com>
2017-12-06 21:47:44 -0500

    Round 2

--- On Submit.js
+++ On Submit.js
@@ -1,6 +1,2 @@
-function changeColor(){
-	var myselect = document.getElementById("spotSelction"),
-	colour = myselect.options[myselect.selectedIndex].className;
-	myselect.className = colour;
-}
+
 

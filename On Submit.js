de412a6d9edbdaf746c92cd841c1e9e581b4a025
adamisrafil <adam.israfil@gmail.com>
2017-12-06 22:05:08 -0500

    linking page to JS

--- On Submit.js
+++ On Submit.js
@@ -1,2 +1,7 @@
-
+function changeColor(color){
+			var myselect = document.getElementById("spotSelction"),
+			spot = myselect.options[myselect.selectedIndex].className;
+			myselect.className = spot;
+			document.spot.style.background-color = color;
+		}
 

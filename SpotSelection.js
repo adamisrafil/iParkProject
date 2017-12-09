24b7ae65d627be22f1af1740a8525c65d1a37813
adamisrafil <adam.israfil@gmail.com>
2017-12-09 17:02:24 -0500

    Made grid creation more effecient with repeat function

--- SpotSelection.js
+++ SpotSelection.js
@@ -8,7 +8,7 @@ $(function() {
 
 	//creating left side of page parking lot
 	var leftLotSpots = 0;
-	$("#leftLot.left_lot_container").css({"grid-template-columns": "1fr 1fr", "grid-template-rows": "3fr 3fr 3fr 3fr 3fr 3fr 3fr 3fr 3fr 3fr"});
+	$("#leftLot.left_lot_container").css({"grid-template-columns": "repeat(2, 1fr)", "grid-template-rows": "repeat(10, 3fr)"});
 		for (var parkingRows = 1; parkingRows < neededParkingRows; parkingRows++) {
 			console.log(parkingRows);
 			for (var parkingColumns = 1; parkingColumns < neededParkingColumns; parkingColumns++) {
@@ -18,16 +18,15 @@ $(function() {
 				// https://stackoverflow.com/questions/10619445/the-preferred-way-of-creating-a-new-element-with-jquery
 				var lotSpace = $('<div id="A' + leftLotSpots + '">A' + leftLotSpots + '</div>');
 				lotSpace.addClass('spot empty');
-				//lotSpace.css({"grid-row": "auto", "grid-column": "auto"});
 				$("#leftLot").append(lotSpace);
 				$("#signUpFormSpotSelection").append('<option id="spotOptionA' + leftLotSpots + '"  value="A' + leftLotSpots + '">A' + leftLotSpots + '</div>');
 				console.log(lotSpace);
 			}
 	}
-	
+
 	//creating right side of page parking lot
 	var rightLotSpots = 0;
-	$("#rightLot.right_lot_container").css({"grid-template-columns": "1fr 1fr", "grid-template-rows": "3fr 3fr 3fr 3fr 3fr 3fr 3fr 3fr 3fr 3fr"});
+	$("#rightLot.right_lot_container").css({"grid-template-columns": "repeat(2, 1fr)", "grid-template-rows": "repeat(10, 3fr)"});
 		for (var parkingRows = 1; parkingRows < neededParkingRows; parkingRows++) {
 			console.log(parkingRows);
 			for (var parkingColumns = 1; parkingColumns < neededParkingColumns; parkingColumns++) {

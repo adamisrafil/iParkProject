5f29274c3e0eaf1d0f1fee66afe2d0c214b36138
adamisrafil <adam.israfil@gmail.com>
2017-12-08 22:33:30 -0500

    Get the user data from the form and using it

--- SpotSelection.js
+++ SpotSelection.js
@@ -1,7 +1,35 @@
+/*
 form.onsubmit = function(){
 	//change class tag from empty to taken
 
 	//create random number for use to identify
 
 }
+*/
+$(function() {
+	$('#signUpForm').on('submit', function(e) {
+		e.preventDefault();
+		var dataFromSignUpForm = $("#signUpForm :input").serializeArray();
+		console.log(dataFromSignUpForm);
+		console.log("print test")
 
+		var firstName = $('#signUpFormFirstName').val();
+		var lastName = $('#signUpFormLastName').val();
+		var spotSelection = $('#signUpFormSpotSelection').val();
+
+		console.log(firstName + lastName + spotSelection);
+
+		//find the gird element corresponding to spotSelection
+		var spot = $('#A1');
+		var spot2 = $('#B7');
+		var spot3 = $('#C7');
+		console.log(spot);
+		console.log(spot2);
+		console.log(spot3);
+		var idSpotSelection = '#' + spotSelection;
+		console.log(idSpotSelection);
+		var spot4 = $(idSpotSelection);
+		console.log(spot4);
+		//
+	});
+});

c039feec53fd5012b992b463c65eb2677ee57f53
adamisrafil <adam.israfil@gmail.com>
2017-12-10 22:02:05 -0500

    Lets use advanced ajax posts with success/failure notifications

--- SpotSelection.js
+++ SpotSelection.js
@@ -1,76 +1,106 @@
 $(function() {
-  var neededParkingRows = 21;
-  var neededParkingColumns = 3;
+	var neededParkingRows = 21;
+	  var neededParkingColumns = 3;
 
-  //creating left side of page parking lot
-  var leftLotSpots = 0;
-  $("#leftLot.left_lot_container").css({
-    "grid-template-columns": "repeat(" + (neededParkingColumns - 1) + ", 1fr)",
-    "grid-template-rows": "repeat(" + (neededParkingRows - 1) + ", 3fr)"
-  });
+	  //creating left side of page parking lot
+	  var leftLotSpots = 0;
+	  $("#leftLot.left_lot_container").css({
+	    "grid-template-columns": "repeat(" + (neededParkingColumns - 1) + ", 1fr)",
+	    "grid-template-rows": "repeat(" + (neededParkingRows - 1) + ", 3fr)"
+	  });
 
-  for (var parkingRows = 1; parkingRows < neededParkingRows; parkingRows++) {
-    console.log(parkingRows);
+	  for (var parkingRows = 1; parkingRows < neededParkingRows; parkingRows++) {
+	    console.log(parkingRows);
 
-    for (var parkingColumns = 1; parkingColumns < neededParkingColumns; parkingColumns++) {
-      console.log(parkingColumns);
-      leftLotSpots++;
-      console.log(leftLotSpots);
-      // https://stackoverflow.com/questions/10619445/the-preferred-way-of-creating-a-new-element-with-jquery
-      var lotSpace = $('<div id="A' + leftLotSpots + '">A' + leftLotSpots + '</div>');
-      lotSpace.addClass('spot empty');
-      $("#leftLot").append(lotSpace);
-      $("#signUpFormSpotSelection").append('<option id="spotOptionA' + leftLotSpots + '"  value="A' + leftLotSpots + '">A' + leftLotSpots + '</div>');
-      console.log(lotSpace);
-    }
-  }
+	    for (var parkingColumns = 1; parkingColumns < neededParkingColumns; parkingColumns++) {
+	      console.log(parkingColumns);
+	      leftLotSpots++;
+	      console.log(leftLotSpots);
+	      // https://stackoverflow.com/questions/10619445/the-preferred-way-of-creating-a-new-element-with-jquery
+	      var lotSpace = $('<div id="A' + leftLotSpots + '">A' + leftLotSpots + '</div>');
+	      lotSpace.addClass('spot empty');
+	      $("#leftLot").append(lotSpace);
+	      $("#signUpFormSpotSelection").append('<option id="spotOptionA' + leftLotSpots + '"  value="A' + leftLotSpots + '">A' + leftLotSpots + '</div>');
+	      console.log(lotSpace);
+	    }
+	  }
 
-  //creating right side of page parking lot
-  var rightLotSpots = 0;
-  $("#rightLot.right_lot_container").css({
-    "grid-template-columns": "repeat(" + (neededParkingColumns - 1) + ", 1fr)",
-    "grid-template-rows": "repeat(" + (neededParkingRows - 1) + ", 3fr)"
-  });
+	  //creating right side of page parking lot
+	  var rightLotSpots = 0;
+	  $("#rightLot.right_lot_container").css({
+	    "grid-template-columns": "repeat(" + (neededParkingColumns - 1) + ", 1fr)",
+	    "grid-template-rows": "repeat(" + (neededParkingRows - 1) + ", 3fr)"
+	  });
 
-  for (var parkingRows = 1; parkingRows < neededParkingRows; parkingRows++) {
-    console.log(parkingRows);
+	  for (var parkingRows = 1; parkingRows < neededParkingRows; parkingRows++) {
+	    console.log(parkingRows);
 
-    for (var parkingColumns = 1; parkingColumns < neededParkingColumns; parkingColumns++) {
-      console.log(parkingColumns);
-      rightLotSpots++;
-      var lotSpace = $('<div id="B' + rightLotSpots + '">B' + rightLotSpots + '</div>');
-      lotSpace.addClass('spot empty');
-      $("#rightLot").append(lotSpace);
-      $("#signUpFormSpotSelection").append('<option id="spotOptionB' + rightLotSpots + '"  value="B' + rightLotSpots + '">B' + rightLotSpots + '</div>');
-      console.log(lotSpace);
-    }
-  }
-  //using the form data on submission to send data to db and change class tag
-  $('#signUpForm').on('submit', function(e) {
-    e.preventDefault();
-    var dataFromSignUpForm = $("#signUpForm :input").serializeArray();
-    console.log(dataFromSignUpForm);
+	    for (var parkingColumns = 1; parkingColumns < neededParkingColumns; parkingColumns++) {
+	      console.log(parkingColumns);
+	      rightLotSpots++;
+	      var lotSpace = $('<div id="B' + rightLotSpots + '">B' + rightLotSpots + '</div>');
+	      lotSpace.addClass('spot empty');
+	      $("#rightLot").append(lotSpace);
+	      $("#signUpFormSpotSelection").append('<option id="spotOptionB' + rightLotSpots + '"  value="B' + rightLotSpots + '">B' + rightLotSpots + '</div>');
+	      console.log(lotSpace);
+	    }
+	  }
+	  //using the form data on submission to send data to db and change class tag
+	  $('#signUpForm').on('submit', function(e) {
+	    e.preventDefault();
+	    var dataFromSignUpForm = $("#signUpForm :input").serializeArray();
+	    console.log(dataFromSignUpForm);
 
-    var firstName = $('#signUpFormFirstName').val();
-    var lastName = $('#signUpFormLastName').val();
-    var spotSelection = $('#signUpFormSpotSelection').val();
+	    var firstName = $('#signUpFormFirstName').val();
+	    var lastName = $('#signUpFormLastName').val();
+	    var spotSelection = $('#signUpFormSpotSelection').val();
 
-    console.log(firstName + " " + lastName + " " + spotSelection);
+	    console.log(firstName + " " + lastName + " " + spotSelection);
 
-    //find the gird element corresponding to spotSelection
-    var idSpotSelection = '#' + spotSelection;
-    console.log(idSpotSelection);
-    var spot4 = $(idSpotSelection);
-    console.log(spot4);
-    //change from empty to taken
-    if ($(idSpotSelection).hasClass('taken')) {
+	    //find the gird element corresponding to spotSelection
+	    var idSpotSelection = '#' + spotSelection;
+	    console.log(idSpotSelection);
+	    var spot4 = $(idSpotSelection);
+	    console.log(spot4);
+	    //change from empty to taken
+	    if ($(idSpotSelection).hasClass('taken')) {
 
-      alert("That spot is taken");
+	      alert("That spot is taken");
 
-    } else {
+	    } else {
 
-      $(idSpotSelection).removeClass('empty').addClass('taken');
-      $.post( "SpotSelection.php", $( "#signUpForm" ).serialize() );
-    }
-  });
+	    	$(idSpotSelection).removeClass('empty').addClass('taken');
+		    //send form data to DB
+		    //verision if its too damn late ill use this
+		    //$.post("SpotSelection.php", $("#signUpForm").serialize());
+
+		    //https://stackoverflow.com/questions/5004233/jquery-ajax-post-example-with-php
+			// Fire off the request to /form.php
+		    var request = $.ajax({
+		        url: "SpotSelection.php",
+		        type: "post",
+		        data: $("#signUpForm").serialize()
+		    });
+
+		    // Callback handler that will be called on success
+		    request.done(function (response, textStatus, jqXHR){
+		        // Log a message to the console
+		        console.log("Hooray, it worked!");
+		    });
+		    
+		    // Callback handler that will be called on failure
+		    request.fail(function (jqXHR, textStatus, errorThrown){
+		        // Log the error to the console
+		        console.error("The following error occurred: " + textStatus, errorThrown);
+		    });
+
+		    // Callback handler that will be called regardless
+		    // if the request failed or succeeded
+		    request.always(function () {
+		        // Reenable the inputs
+		        console.log("Always");
+   	 		});
+
+	    }
+  	});
 });
